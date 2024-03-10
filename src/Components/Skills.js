96319c19e4b8d12f9f87d75fb52571b770a767f0
khushboosharma2003 <khushboo.261203@gmail.com>
2024-03-10 15:07:38 +0530

    my new commit

---
+++ src/Components/Skills.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Fotter from './Fotter'
+export default function Skills() {
+  return (
+    <div>
+      {/*
+        <Navbar /> */}
+        <div className="skill">
+
+    <h4>TECHNICAL and PROFESSIONAL</h4>
+    <h1>SKILLS</h1>
+    <div className="container">
+      <div className="sleft">
+        <h3>TECHNICAL SKILLS</h3>
+        <div className="skill-box">
+          <h4>Python</h4>
+          <h5>70%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>HTML</h4>
+          <h5>90%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>CSS</h4>
+          <h5>70%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>C</h4>
+          <h5>80%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>C++</h4>
+          <h5>80%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>JS</h4>
+          <h5>70%</h5>
+        </div>
+
+
+      </div>
+      <div className="sright">
+        <h3>PROFESSIONAL SKILLS</h3>
+        <div className="skill-box">
+          <h4>Problem Solving</h4>
+          <h5>90%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>Team Working</h4>
+          <h5>90%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>Creativity</h4>
+          <h5>80%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>Project Management</h4>
+          <h5>80%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>Time Management</h4>
+          <h5>70%</h5>
+        </div>
+        <div className="skill-box">
+          <h4>Logical</h4>
+          <h5>70%</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="achieve">
+    <h1>Achievements </h1>
+    <div class="cards">
+    <div className="card">
+      
+      1. Completed Python programming language course on Udemy (NOVEMBER 2023)
+      <a className="certi" href="https://drive.google.com/file/d/1ZYTO2UD1fRiJF8UR7-O01uylRwqrMSau/view?usp=sharing">Link</a>
+
+      </div>
+      <div className="card">
+
+        2. Completed JAVASCRIPT course from UDEMY (MAY 2023 )
+        <a className="certi" href="https://drive.google.com/file/d/1QLrblBA04ZaxJCH_zZdo91BE8s8Zc5uM/view?usp=sharing">Link</a>
+
+      </div>
+      <div className="card">
+
+        3. Attended 3-day JS-REACT workshop organized by IIT, Kanpur in February 2023
+        <a className="certi" href="https://drive.google.com/file/d/12eRMdjexAPGVHfxgdHANlOS1YvBwhNf-/view?usp=sharing">Link</a>
+
+      </div>
+      <div className="card">
+
+        4. Attended 6-day C Programming workshop, also hosted by IIT, Kanpur in June 2022
+        <a className="certi" href="https://drive.google.com/file/d/1sjORJqP1ja8KrO7sTlthXxCNRhm0SSWc/view?usp=sharing">Link</a>
+
+      </div>
+      <div className="card">
+
+        5. Took part in a variety of Personal Development Program (PDP) activities, securing positions that highlight
+        active participation
+
+      </div>
+    </div>
+    </div>
+  </div>
+
+  )
+  }
